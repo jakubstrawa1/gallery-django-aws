dc2e7186df2b7c8fd3809b0981d9c1a88bfad1ca
Jakub Strawa <93473615+jakubstrawa1@users.noreply.github.com>
2021-11-03 23:51:26 +0100

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -131,9 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
@@ -146,3 +147,5 @@ AWS_SECRET_ACCESS_KEY = '6ZH7vQ5BBaM3mmJi37Ww4dvLMVdQWEocIxQsqpfx'
 
 AWS_STORAGE_BUCKET_NAME = 'photoshare-kuba'
 AWS_QUERYSTRING_AUTH = False
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
