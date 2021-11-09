688abfd35566cdd805af5cdd8c85c2eb39d081f9
Jakub Strawa <93473615+jakubstrawa1@users.noreply.github.com>
2021-11-09 22:09:55 +0100

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3bigk-)+7%qw8)8yu738k5$-47r3*w1@8&et7@&+@j#@_2g9(p'
+SECRET_KEY = 'secretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
